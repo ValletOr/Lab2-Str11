0252c33b7040c34936de43437de1e5c20ee67ca0
ValletOr <mtvac11@gmail.com>
2021-09-22 19:20:49 +0800

    Вставил логику из предыдущей лабораторной.

--- Lab2 Str11/Form1.Designer.cs
+++ Lab2 Str11/Form1.Designer.cs
@@ -78,6 +78,7 @@ namespace Lab2_Str11
             this.button1.TabIndex = 4;
             this.button1.Text = "Проверить!";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
--- Lab2 Str11/Form1.cs
+++ Lab2 Str11/Form1.cs
@@ -17,5 +17,54 @@ namespace Lab2_Str11
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public static class Logic
+    {
+        public static string Execution(string word1, string word2)
+        {
+            string outMessage = "", buffer = "";
+            bool trigger = false;
+
+            //Чистка первого слова от повторяющихся букв
+            for (int i = 0; i < word1.Length; i++)
+            {
+                if (!buffer.Contains(word1[i]))
+                {
+                    buffer += word1[i];
+                }
+            }
+            word1 = buffer;
+
+            //Основной расчёт
+            for (int i = 0; i < word1.Length; i++)
+            {
+                trigger = false;
+                for (int j = 0; j < word2.Length; j++)
+                {
+                    if (word1[i] == word2[j])
+                    {
+                        trigger = true;
+                        outMessage += "да";
+                        break;
+                    }
+
+                }
+                if (!trigger)
+                {
+                    outMessage += "нет";
+                }
+                if (i != word1.Length - 1)
+                {
+                    outMessage += " ";
+                }
+            }
+
+            return outMessage;
+        }
     }
 }
