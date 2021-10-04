489a5460e76e942a20cf6402dd512da0f9c67281
ValletOr <mtvac11@gmail.com>
2021-10-04 22:39:50 +0800

    Добавил автотесты

---
+++ Lab2 Str11Tests/LogicTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab2_Str11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_Str11.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void NoMatchesNoRepitition()
+        {
+            string word1 = "Процессор";
+            string word2 = "Синхрофазотрон";
+
+            Assert.AreEqual(Logic.Execution(word1, word2), "п - нет\r\nр - да\r\nо - да\r\nц - нет\r\nе - нет\r\nс - да");
+        }
+
+        [TestMethod()]
+        public void MatchesAndRepetition()
+        {
+            string word1 = "Телега";
+            string word2 = "Яд";
+
+            Assert.AreEqual(Logic.Execution(word1, word2), "т - нет\r\nе - нет\r\nл - нет\r\nг - нет\r\nа - нет");
+        }
+    }
+}
